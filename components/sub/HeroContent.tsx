5066a4dcbe4f4e8b3b0351b673474f3a1050769e
tmachine <tmachine@sys08.PEC.IN>
2024-03-21 08:53:39 +0530

    sm corr

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -14,7 +14,7 @@ const HeroContent = () => {
     <motion.div
       initial="hidden"
       animate="visible"
-      className="flex flex-row items-center justify-center px-20 mt-[30vh] w-full z-[20]"
+      className="flex flex-row items-center justify-center px-20 mt-[30vh] 2xl:mt-[55vh] w-full z-[20]"
     >
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
         
